c194eeecc7da56ad0b411d7022b123cc74c46bc7
abing <binglianghuang@gmail.com>
2016-06-13 17:34:32 +0800

    protocol走配置

--- ZPHP/Server/Adapter/Http.php
+++ ZPHP/Server/Adapter/Http.php
@@ -15,11 +15,8 @@ class Http implements IServer
 
     public function run()
     {
-        if ($_SERVER['HTTP_CONTENT_ENCODING'] == "gzip") {
-            Protocol\Request::setServer(Protocol\Factory::getInstance('GzHttp'));
-        } else {
-            Protocol\Request::setServer(Protocol\Factory::getInstance('Http'));
-        }
+        $protocol = Core\Config::getField('Project', 'protocol', 'Http');
+        Protocol\Request::setServer(Protocol\Factory::getInstance($protocol));
         Protocol\Request::parse($_REQUEST);
         return Core\Route::route();
     }
